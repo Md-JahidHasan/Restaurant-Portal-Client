36c713cc34bd9f8c763e9cb3966e72af149cca85
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2024-11-08 23:36:28 +0600

    Menu Page implemented

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -2,9 +2,14 @@ import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 
 import image from "../../../assets/menu/banner3.jpg";
-import PopularItems from "../../Home/PopularItems/PopularItems";
+// import PopularItems from "../../Home/PopularItems/PopularItems";
+import desertImage from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImage from "../../../assets/menu/pizza-bg.jpg";
+import saladImage from "../../../assets/menu/salad-bg.jpg";
+import soupImage from "../../../assets/menu/soup-bg.jpg";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import MenuCategory from "../../MenuCategory/MenuCategory";
 
 const Menu = () => {
   const [menu] = useMenu();
@@ -27,24 +32,42 @@ const Menu = () => {
         subTitle="Would You Like To Try A Dish?"
       ></Cover>
 
+      {/* ==========Main Cover======== */}
       <SectionTitle
         subHeading="Don't miss"
         heading="Tosays Offer"
       ></SectionTitle>
 
-      <PopularItems></PopularItems>
-      <Cover
-        image={image}
-        title="OUR MENU"
-        subTitle="Would You Like To Try A Dish?"
-      ></Cover>
-      <PopularItems></PopularItems>
-      <Cover
-        image={image}
-        title="OUR MENU"
-        subTitle="Would You Like To Try A Dish?"
-      ></Cover>
-      <PopularItems></PopularItems>
+      {/* ====offered menu items===== */}
+      <MenuCategory items={offered}></MenuCategory>
+
+      {/* ========Dessert Items======= */}
+      <MenuCategory
+        items={dessert}
+        title="Desserts"
+        coverImage={desertImage}
+        subTitle="lLorem Ipsum is simply dummy text research work"
+      ></MenuCategory>
+
+      <MenuCategory
+        items={pizza}
+        title="PIZZA"
+        subTitle="lLorem Ipsum is simply dummy text research work"
+        coverImage={pizzaImage}
+      ></MenuCategory>
+
+      <MenuCategory
+        items={salad}
+        title="SALAD"
+        subTitle="lLorem Ipsum is simply dummy text research work.dummy text research work"
+        coverImage={saladImage}
+      ></MenuCategory>
+      <MenuCategory
+        items={soup}
+        title="SOUP"
+        subTitle="lLorem Ipsum is simply dummy text research work.dummy text research work"
+        coverImage={soupImage}
+      ></MenuCategory>
     </div>
   );
 };
--- src/pages/MenuCategory/MenuCategory.jsx
+++ src/pages/MenuCategory/MenuCategory.jsx
@@ -1,5 +1,19 @@
-const MenuCategory = () => {
-  return <div></div>;
+import Cover from "../Shared/Cover/Cover";
+import MenuItem from "../Shared/MenuItems/MenuItem";
+
+const MenuCategory = ({ items, coverImage, title, subTitle }) => {
+  return (
+    <div>
+      {title && (
+        <Cover image={coverImage} title={title} subTitle={subTitle}></Cover>
+      )}
+      <div className="grid md:grid-cols-2 gap-10 my-11 mx-4">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
